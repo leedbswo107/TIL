69d7551491f23509f8f648edffce9a065709e592
leedbswo107 <ljyu1@naver.com>
2024-11-04 16:39:18 +0900

    delete: duplicate files

--- BaekJoon/Front-End/NodeJs/module/cluster.js
+++
@@ -1,8 +0,0 @@
-import * as cluster from 'cluster';
-
-// let cluster = require('cluster');
-// Round Robin 방식으로 스케쥴링
-cluster.schedulingPolicy = cluster.SCHED_RR;
-
-if (cluster.isMaster === true) {
-}
