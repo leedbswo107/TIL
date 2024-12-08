a33a593df99bb601492eacd30046fd54d98a2a1a
leedbswo107 <ljyu1@naver.com>
2024-12-08 16:37:55 +0900

    rename: name convention apply

---
+++ TypeScript/Variable/null-undefined.ts
@@ -0,0 +1,3 @@
+// null, undefined 의 사용
+let a: null = null;
+let b: undefined = undefined;
---
+++ TypeScript/Variable/tuple.ts
@@ -0,0 +1,2 @@
+// tuple 의 사용 (배열 + 인덱스별 타입 지정)
+let tuple1: [number, string, boolean] = [1, "Good!", true];
