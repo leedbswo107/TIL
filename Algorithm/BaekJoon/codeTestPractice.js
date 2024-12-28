d101c9ab043f705db1df2fa5e97fe4a2af05327b
leedbswo107 <ljyu1@naver.com>
2024-12-28 21:44:30 +0900

    feat: 9461

--- Algorithm/BaekJoon/codeTestPractice.js
+++ Algorithm/BaekJoon/codeTestPractice.js
@@ -4788,6 +4788,37 @@
 // 여기
 console.log("here");
 
+// 10816번 문제 푸는중. 이진탐색
+// let [N, myCards, M, numCards] = require("fs")
+//   .readFileSync("./input.txt", "utf-8")
+//   .trim()
+//   .split("\n");
+// const result = new Array(+M);
+// myCards = myCards
+//   .split(" ")
+//   .map(Number)
+//   .sort((a, b) => a - b);
+// numCards = numCards.split(" ").map(Number);
+// const solution = (data, target) => {
+//   const arr = [...data];
+//   let start = 0;
+//   let end = arr.length - 1;
+//   let mid = Math.floor((start + end) / 2);
+//   while (start <= end) {
+//     if (arr[mid] === target) {
+//       if (arr[start] !== target) start++;
+//       else if (arr[end] !== target) end--;
+//       else return end - start + 1;
+//     } else {
+//       arr[mid] < target ? (start = mid + 1) : (end = mid - 1);
+//       mid = Math.floor((start + end) / 2);
+//     }
+//   }
+//   return 0;
+// };
+// numCards.forEach((el, i) => (result[i] = solution(myCards, el)));
+// console.log(result.join(" "));
+
 // 16922번 문제 푸는중
 // const N = +require("fs").readFileSync("./input.txt", "utf-8").trim();
 // const ROME = [1, 5, 10, 50];
@@ -6163,28 +6194,6 @@ console.log("here");
 // };
 // while (i <= N) eratosthenes(i++);
 // console.log(result.join("\n"));
-
-// 10816번 문제 푸는중. 이진탐색
-// const arr = require("fs")
-//   .readFileSync("./input.txt", "utf-8")
-//   .trim()
-//   .split("\n");
-// const numbers = arr[1]
-//   .split(" ")
-//   .map(Number)
-//   .sort((a, b) => a - b);
-// const numberM = arr[3].split(" ").map(Number);
-// const result = new Array(numberM.length).fill(0);
-// const half = numberM.forEach((el, i) =>
-//   numbers.filter((e) => e === el && result[i]++)
-// );
-// console.log(result.join(" "));
-
-// numberM.forEach((e, i) => {
-//   for (let j = 0; j < numbers.length; j++) {
-//     if (e === numbers[j]) result[i] += 1;
-//   }
-// });
 
 // 2563번 문제 푸는중..
 // const [N, ...arr] = require("fs")
