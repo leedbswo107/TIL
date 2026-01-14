3c7a22812c81f088eeeed464823221d52d02aa9d
Michael <54614330+leedbswo107@users.noreply.github.com>
2026-01-14 13:24:11 +0900

    feat: 27964
    Implement solution to determine if a pizza can be made with given toppings.

---
+++ Algorithm/BaekJoon/27964.js
@@ -0,0 +1,13 @@
+/**
+ * 문제
+ * 치즈와 피자에 환장하는 비행씨는 매일같이 치즈피자를 사 먹다가 지갑이 거덜 나고 말았다. 만들어 먹는 것이 사 먹는 것보다 싸다는 것을 안 비행씨는 여러 가지 토핑을 가져와서 직접 피자를 만들어 먹기로 했다.
+ * 콰트로치즈피자는 이름 그대로, 서로 다른 네 종류의 치즈가 토핑으로 들어가야 한다. 수많은 치즈피자를 먹어 온 비행씨는 토핑의 이름이 Cheese로 끝나면 이 토핑이 치즈라는 사실을 알고 있다. 비행씨가 가져온 토핑의 목록을 보고, 이 토핑의 일부 혹은 전부를 이용하여 콰트로치즈피자를 만들 수 있는지 답해 보자.
+ * 
+ * 출력
+ * 입력으로 주어진 토핑의 목록으로 콰트로치즈피자를 만들 수 있으면 yummy, 만들 수 없으면 sad를 출력하라.
+ */
+const input = require('fs').readFileSync(0, 'utf-8').trim().split('\n');
+const N = +input[0];
+const target = 'Cheese';
+const toppings = new Set(input[1].split(' ').filter(v => (v.slice(v.length - 6) === target))).size;
+console.log(toppings >= 4 ? 'yummy' : 'sad');
