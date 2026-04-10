c352dfa32ddc4bcc85cee6df96b63357fcb144ab
Michael <54614330+leedbswo107@users.noreply.github.com>
2026-04-10 21:41:49 +0900

    add: 23811

---
+++ Algorithm/BaekJoon/23811.js
@@ -0,0 +1,5 @@
+const N = +require('fs').readFileSync(0, 'utf-8').trim();
+const p1 = ('@'.repeat(N * 5) + '\n').repeat(N);
+const p2 = ('@'.repeat(N) + '\n').repeat(N);
+const result = p1 + p2 + p1 + p2 + p1;
+console.log(result);
